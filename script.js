970b472c4662168aaa15c0b8faa42f3e1a28dc27
es3017 <78827803+es3017@users.noreply.github.com>
2021-02-11 16:29:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var name = prompt("what is your name?");
+var color = prompt("what is your favorite color?");
+document.body.querySelector(".message").innerHTML="My name is " +name+". my favorite color is "+color;
